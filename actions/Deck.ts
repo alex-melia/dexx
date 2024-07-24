5675648e60b249c305646e1ed4aaa4c887745a3a
alex-melia <alexmelia41@gmail.com>
2024-07-24 17:21:12 +0100

    Update

--- actions/Deck.ts
+++ actions/Deck.ts
@@ -160,6 +160,7 @@ export async function getUserDecks() {
   const session = await auth()
 
   console.log(session?.user.id)
+  console.log(session?.user.id)
 
   try {
     const decks = await prisma.deck.findMany({
